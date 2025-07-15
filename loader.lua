ce97d20eab3be69d44cf4451a30b40d643cf72e4
andrewdarkyywastaken <darkyy_andrew_darkyy@protonmail.com>
2025-07-15 20:23:26 +0300

    Fix bugs

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
-if not game:IsLoaded()then
-	game.Loaded:Wait();
+if type(script_key)~="string"or #script_key~=32 then
+	return messagebox("Copy the entire script that was provided to you by the Luarmor bot. Ensure that it consists of precisely 2 lines of code.","Moonlight",0x00000030+0x00010000);
 end;
-if getfflag and getfflag("DebugRunParallelLuaOnMainThread")=="true"then
+if getfflag("DebugRunParallelLuaOnMainThread")=="true"then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/219cc6b2c3578ead9cf25bac3c007dbb.lua"))();
 else
 	run_on_actor(game:GetService("ReplicatedFirst"):FindFirstChildOfClass("Actor"),`script_key="{script_key}";loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/219cc6b2c3578ead9cf25bac3c007dbb.lua"))();`);
