b4d52129d12e91d50b7c23aa3b5956c3c6b15e99
andrewdarkyywastaken <darkyy_andrew_darkyy@protonmail.com>
2025-07-15 20:07:32 +0300

    Fix bugs

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 if not game:IsLoaded()then
 	game.Loaded:Wait();
 end;
-if getfflag("DebugRunParallelLuaOnMainThread")=="true"then
+if getfflag and getfflag("DebugRunParallelLuaOnMainThread")=="true"then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/219cc6b2c3578ead9cf25bac3c007dbb.lua"))();
 else
 	run_on_actor(game:GetService("ReplicatedFirst"):FindFirstChildOfClass("Actor"),`script_key="{script_key}";loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/219cc6b2c3578ead9cf25bac3c007dbb.lua"))();`);
