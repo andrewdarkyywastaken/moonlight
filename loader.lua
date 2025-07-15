62f6ec8dd1a22edd0b9de1097f06b08efa94c5fb
andrewdarkyywastaken <darkyy_andrew_darkyy@protonmail.com>
2025-07-15 19:53:50 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(teleportState,placeId)
+	if teleportState==Enum.TeleportState.InProgress and placeId==game.PlaceId then
+		queue_on_teleport(`script_key="{script_key}"loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewdarkyywastaken/moonlight/main/loader.lua"))();`);
+	end;
+end);
+if getfflag("DebugRunParallelLuaOnMainThread")=="true"then
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/219cc6b2c3578ead9cf25bac3c007dbb.lua"))();
+else
+	run_on_actor(game:GetService("ReplicatedFirst"):FindFirstChildOfClass("Actor"),`script_key="{script_key}";loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/219cc6b2c3578ead9cf25bac3c007dbb.lua"))();`);
+end;
